eec473b0c29b30327ed290185147dd31ec8edd4e
Harena Rico <mahefaniainarindra@gmail.com>
2025-09-21 20:56:18 +0300

    configure corsheaders

--- core/settings.py
+++ core/settings.py
@@ -36,10 +36,12 @@ INSTALLED_APPS = [
     'authentification',
     # third app
     'rest_framework',
-    'rest_framework_simplejwt.token_blacklist'
+    'rest_framework_simplejwt.token_blacklist',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,6 +132,10 @@ REST_FRAMEWORK={
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    env.str("FRONT_URL")
+]
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(
